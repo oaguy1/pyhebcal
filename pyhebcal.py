033e3411332ea96207b078ac3d420346b60335fb
oaguy1 <oaguy1@gmail.com>
2014-08-20 18:47:15 -0400

    Add source file with basic constants

---
+++ pyhebcal.py
@@ -0,0 +1,32 @@
+'''
+file: pyhebcal.py
+
+library for using hebrew dates
+'''
+
+class HebCal(object):
+
+    # Important constants
+    CURRENT_DATE = 0
+    CURRENT_MONTH = 0
+    CURRENT_YEAR = 0
+    
+    HEBREW_EPOCH = -1373429
+    
+    JANUARY = 1
+    DECEMBER = 12
+
+    MONTH_NISAN = 1
+    MONTH_IYAR = 2
+    MONTH_SIVAN = 3
+    MONTH_TAMMUZ = 4
+    MONTH_AV = 5
+    MONTH_ELUL = 6
+    MONTH_TISHREI = 7
+    MONTH_CHESHVAN = 8
+    MONTH_KISLEV = 9
+    MONTH_TEVET = 10
+    MONTH_SHAVAT = 11
+    MONTH_ADAR = 12
+    MONTH_ADARII = 13
+
